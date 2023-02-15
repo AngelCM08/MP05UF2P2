89cb35ef5eef59bbceba4efd05b009d3d2004a9c
Ángel <angelcastromerino@gmail.com>
2023-02-15 19:43:48 +0100

    First commit

--- src/main/java/ex1/Enunciat.java
+++ src/main/java/ex1/Enunciat.java
@@ -15,7 +15,13 @@ public class Enunciat {
         3.2. Mostra el log de GitHub, en concret la línia on es veuen les proves executades, per a demostrar que
              l'execució de proves sobre GitHub funciona correctament.
 
+             Al executar les actions per primer cop, després d'haver activat els permisos de lectura i escriptura del
+             Workflow a Settings/Actions, també he habilitat el dependency graph perque em sortia un error.
+             https://docs.github.com/en/code-security/supply-chain-security/understanding-your-software-supply-chain/configuring-the-dependency-graph
+
         3.3. Explica en què consisteix el concepte de Continuous Integration (CI) i quina relació té amb el que
              acabes de fer en aquest exercici.
+
+            //TODO xdddddddddddddddddddddddddddddddddddddddddddd
 */
 }
